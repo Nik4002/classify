dc76dea99916dcbca35020c6b9c22141128acbb8
Nik4002 <pateln4002@gmail.com>
2022-08-03 16:52:31 -0500

    Cargo clippy + fmt

--- src/head_tail.rs
+++ src/head_tail.rs
@@ -13,7 +13,7 @@ use crate::utilities::Classification;
 /// use classify::get_head_tail_classification;
 /// use classify::{Classification, Bin};
 ///
-/// let data: Vec<f64> = vec![1.0/1.0, 1.0/2.0, 1.0/3.0, 1.0/4.0, 1.0/5.0, 
+/// let data: Vec<f64> = vec![1.0/1.0, 1.0/2.0, 1.0/3.0, 1.0/4.0, 1.0/5.0,
 ///                           1.0/6.0, 1.0/7.0, 1.0/8.0, 1.0/9.0, 1.0/10.0];
 ///
 /// let result: Classification = get_head_tail_classification(&data);
@@ -41,7 +41,7 @@ pub fn get_head_tail_classification(data: &Vec<f64>) -> Classification {
 /// ```
 /// use classify::get_head_tail_breaks;
 ///
-/// let data: Vec<f64> = vec![1.0/1.0, 1.0/2.0, 1.0/3.0, 1.0/4.0, 1.0/5.0, 
+/// let data: Vec<f64> = vec![1.0/1.0, 1.0/2.0, 1.0/3.0, 1.0/4.0, 1.0/5.0,
 ///                           1.0/6.0, 1.0/7.0, 1.0/8.0, 1.0/9.0, 1.0/10.0];
 ///
 /// let result: Vec<f64> = get_head_tail_breaks(&data);
@@ -57,14 +57,16 @@ pub fn get_head_tail_breaks(data: &Vec<f64>) -> Vec<f64> {
     }
     sorted_data.sort_by(|a, b| a.partial_cmp(b).unwrap());
     head_tail_recursion(&sorted_data, &mut breaks);
-    
+
     breaks
 }
 
 /// Recursive function used by get_head_tail_breaks that populates a vector of breaks according to the head-tail breaks algorithm
 pub fn head_tail_recursion(data: &Vec<f64>, breaks: &mut Vec<f64>) {
     let mut mean: f64 = 0.0;
-    for val in data {mean += val};
+    for val in data {
+        mean += val
+    }
     mean /= data.len() as f64;
 
     breaks.push(mean);
@@ -73,7 +75,7 @@ pub fn head_tail_recursion(data: &Vec<f64>, breaks: &mut Vec<f64>) {
     let mut low = 0;
     let mut high = data.len();
     'outer: loop {
-        let mid = (low + high)/2;
+        let mid = (low + high) / 2;
         if mean < data[mid] as f64 {
             high = mid;
         } else if mean == data[mid] {
@@ -89,9 +91,10 @@ pub fn head_tail_recursion(data: &Vec<f64>, breaks: &mut Vec<f64>) {
 
     let head: Vec<f64> = data[break_idx..].to_vec();
 
-    if head.len() > 0 {
-        if (head.len() as f64)/(data.len() as f64) <= 0.4 && head[0] != head[head.len()-1] {
-            head_tail_recursion(&head, breaks);
-        }
+    if !head.is_empty()
+        && (head.len() as f64) / (data.len() as f64) <= 0.4
+        && head[0] != head[head.len() - 1]
+    {
+        head_tail_recursion(&head, breaks);
     }
 }
