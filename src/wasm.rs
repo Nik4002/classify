f1d9d509d9c9579c2f3c3e87a9e56cd6b611226d
Nik4002 <pateln4002@gmail.com>
2022-08-12 14:03:03 -0500

    Tentative wasm implementation

--- src/wasm.rs
+++ src/wasm.rs
@@ -5,7 +5,7 @@ use wasm_bindgen::prelude::*;
 pub struct JSBin {
     pub bin_start: f64,
     pub bin_end: f64,
-    pub count: i64,
+    pub count: u64,
 }
 
 #[wasm_bindgen]
@@ -28,7 +28,73 @@ impl From<&Bin> for JSBin {
 }
 
 #[wasm_bindgen]
-pub fn get_jenks_breaks(no_bins: usize, data: &[f64]) -> JsValue {
-    let breaks: JSClassification = crate::jenks::get_jenks_classification(no_bins, data).into();
-    breaks.into()
+pub fn breaks_to_classification(breaks: &[f64], data: &[f64]) -> JSClassification { // Should this be JSClassification or JsValue?
+    let class: Classification = crate::utilities::breaks_to_classification(breaks, data);
+    class.into()
+}
+
+#[wasm_bindgen]
+pub fn classify_val(val: f64, class: &JSClassification) -> Option<usize> { // This doesn't work because classify_val isn't equipped to handle JSClassification
+    let bin: Option<usize> = crate::utilities::classify_val(val, class as Classification);
+    bin
+}
+
+#[wasm_bindgen]
+pub fn get_jenks_breaks(no_bins: usize, data: &[f64]) -> Box<[f64]> { // Should these be Box<[f64]> or something else? (Maybe JsValue?)
+    let breaks: [f64] = crate::jenks::get_jenks_breaks(no_bins, data);
+    breaks.into_boxed_slice()
+}
+
+#[wasm_bindgen]
+pub fn get_jenks_classification(no_bins: usize, data: &[f64]) -> JsValue { // What's the benefit of using JsValue instead of JSClassification here?
+    let class: JSClassification = crate::jenks::get_jenks_classification(no_bins, data).into();
+    class.into()
+}
+
+#[wasm_bindgen]
+pub fn get_quantile_breaks(no_bins: usize, data: &[f64]) -> Box<[f64]> {
+    let breaks: [f64] = crate::quantile::get_quantile_breaks(no_bins, data);
+    breaks.into_boxed_slice()
+}
+
+#[wasm_bindgen]
+pub fn get_quantile_classification(no_bins: usize, data: &[f64]) -> JsValue {
+    let class: JSClassification = crate::quantile::get_quantile_classification(no_bins, data).into();
+    class.into()
+}
+
+#[wasm_bindgen]
+pub fn get_head_tail_breaks(data: &[f64]) -> Box<[f64]> {
+    let breaks: [f64] = crate::head_tail::get_head_tail_breaks(data);
+    breaks.into_boxed_slice()
+}
+
+#[wasm_bindgen]
+pub fn get_head_tail_classification(data: &[f64]) -> JsValue {
+    let class: JSClassification = crate::head_tail::get_head_tail_classification(data).into();
+    class.into()
+}
+
+#[wasm_bindgen]
+pub fn get_equal_interval_breaks(no_bins: usize, data: &[f64]) -> Box<[f64]> {
+    let breaks: [f64] = crate::equal_interval::get_equal_interval_breaks(no_bins, data);
+    breaks.into_boxed_slice()
+}
+
+#[wasm_bindgen]
+pub fn get_equal_interval_classification(no_bins: usize, data: &[f64]) -> JsValue {
+    let class: JSClassification = crate::equal_interval::get_equal_interval_classification(no_bins, data).into();
+    class.into()
+}
+
+#[wasm_bindgen]
+pub fn get_standard_deviation_breaks(bin_size: f64, data: &[f64]) -> Box<[f64]> {
+    let breaks: [f64] = crate::standard_deviation::get_st_dev_breaks(bin_size, data);
+    breaks.into_boxed_slice()
+}
+
+#[wasm_bindgen]
+pub fn get_standard_deviation_classification(bin_size: f64, data: &[f64]) -> JsValue {
+    let class: JSClassification = crate::standard_deviation::get_st_dev_classification(bin_size, data).into();
+    class.into()
 }
